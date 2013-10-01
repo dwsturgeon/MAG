853f453b4e574f16b6c392261e360f1049e3b2bb
Devin Sturgeon <dwsturgeon@mediafu.se>
2013-09-30 23:56:46 -0400

    INIT

---
+++ MAG/MAGMasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MAGMasterViewController.h
+//  MAG
+//
+//  Created by Devin Sturgeon on 9/30/13.
+//  Copyright (c) 2013 Devin Sturgeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAGMasterViewController : UITableViewController
+
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
+
+@end
